21053ec42c46dc8f9fd5472b56c4ef2b701b1425
Nguyễn Quý Dương <71910963+duongquy2404@users.noreply.github.com>
2021-09-26 17:01:54 +0700

    xóa bài làm trên lớp

--- FavoriteNumbers.py
+++
@@ -1,3 +0,0 @@
-numbers={'Anh':3,'Dung':5,'Cuong':8,'Phuc':9,'Quan':2}
-for i in numbers:
-    print(f"So yeu thich cua {i} la: {numbers[i]}")
--- RestaurantSeating.py
+++
@@ -1,8 +0,0 @@
-print("Ban muon dat ban may nguoi!")
-
-n=(int)(input())
-
-if n<=8:
-    print("Ban cua ban da san sang!")
-else:
-    print("Vui long cho khi co ban!")
